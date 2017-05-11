d55e29cf93dba9053bf3cbc92551613d2c513ba6
Tomer Ovadia <tomertomertomer@gmail.com>
2017-05-11 17:07:20 -0400

    Complete exceptions and static assets

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,29 @@
 require 'erb'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      app.call(env)
+    rescue Exception => e
+      # render_exception(e)
+      return ['500', {'Content-type' => 'text/html'}, [render_exception(e)]]
+    end
   end
 
   private
 
   def render_exception(e)
+    path = "lib/templates/rescue.html.erb"
+    raw_contents = File.read(path)
+    erb_contents = ERB.new(raw_contents)
+    html_content = erb_contents.result(binding)
+    html_content
   end
 
 end
