412d4a33647243381c50ec6415f17ae178007c91
Tomer Ovadia <tomertomertomer@gmail.com>
2017-05-14 16:47:44 -0400

    Add Force Error button

--- controllers/vending_machines_controller.rb
+++ controllers/vending_machines_controller.rb
@@ -5,7 +5,6 @@ Dir[File.dirname(__FILE__) + "/../models/*.rb"].each {|file| require_relative fi
 class VendingMachinesController < ControllerBase
 
   def show
-    itemsFromCookie = session['vending_machine']
 
     @name_to_image_filename = {
       skittles: 'skittles.jpg',
@@ -13,15 +12,16 @@ class VendingMachinesController < ControllerBase
       cheetos: 'cheetos.png',
       'm&ms': 'm&ms.jpeg',
       'swedish fish': 'swedish_fish.jpeg',
-      'starburst': 'starburst.png',
+      starburst: 'starburst.png',
       'cheez-its': 'cheez_its.jpg',
     }
+
+    itemsFromCookie = session['vending_machine']
 
     @vending_machine = VendingMachine.new(itemsFromCookie)
 
     @user_mode = params['user-mode'] || session['user_mode'] || 'customer'
     session['user_mode'] = @user_mode
-
 
     # refresh cookie in case default items were used to create VendingMachine
     session['vending_machine'] = @vending_machine.itemsHash
